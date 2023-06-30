6e24fdd9ecb7f06ed47f73da2c9f2b186f0ac1a4
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-30 17:57:03 -0400

    Prompts exist now

--- index.js
+++ index.js
@@ -2,13 +2,39 @@ const inquirer = require("inquirer");
 
 
 // Time for prompts
-// Text up to 3 characters
+inquirer
+    .prompt([
+        // Text up to 3 characters
+        {
+            name: "logoText",
+            message: "Enter up to three characters:",
+            // This should check if the input is less than or equal to 3 characters
+            validate: (input) => {
+                return input.length <= 3;
+            },
+        },
 
+        // text color by keyword or hex number
+        {
+            name: "textColor",
+            message: "Enter your text color (keyword or hexadecimal):",
+        },
 
-// text color by keyword or hex number
+        // shape with list: circle, triangle, square
+        {
+            name: "logoShape",
+            type: "list",
+            message: "Choose a shape:",
+            choices: ["circle", "triangle", "square"],
+        },
 
-
-// shape with list: circle, triangle, square
-
-
-// shape color by keyword or hex number
+        // shape color by keyword or hex number
+        {
+            name: "shapeColor",
+            message: "Enter your shape color (keyword or hexadecimal):",
+        },
+    ])
+    .then((answers) => [
+        // Generate the logo based on user input, something like:
+        // const logo = generateLogo(answers);
+    ]);
