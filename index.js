3250f3eee8c1b5913fcff8aceead2817921ee6e5
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-30 18:32:45 -0400

    Shape templates exist now

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function generateLogo(answers) {
     // circle has attributes cx, cy, r, fill
         // cx is x-coordinate of circle center
         // cy is y-coordinate of circle center
-        // r is radius
+     // r is radius
         // fill is fill color
     // polygon has attributes of points, fill
         // takes a list of x,y coordinate pairs separated by spaces
@@ -57,6 +57,20 @@ function generateLogo(answers) {
         // y is y of top-left
         // width and height are semantic, like fill
     // All the same, I had ChatGPT design the shapes to ensure they fit within the required 300x200px image. I understand the circle and square well enough to try it myself, but the triangle is witchcraft as far as I can tell. ¯\_(ツ)_/¯
+
+    switch (logoShape) {
+        case "cirlce":
+            svgTemplate = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
+            break;
+        case "triangle":
+            svgTemplate = `<polygon points="150,25 75,175 225,175" fill="${shapeColor}" />`;
+            break;
+        case "square":
+            svgTemplate = `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`;
+            break;
+        default:
+            break;
+    }
 
     
 }
