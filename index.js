9ddd904ab2711576427e8255237ef2f9fbd88150
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-30 18:53:07 -0400

    Installed express so I can open localhost

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const { createSVG } = require("svg.js");
 
 
 // Time for prompts
@@ -43,37 +44,12 @@ inquirer
 function generateLogo(answers) {
     const { logoText, textColor, logoShape, shapeColor } = answers;
 
-    // Okay so I kinda get how svg shapes work.
-    // circle has attributes cx, cy, r, fill
-        // cx is x-coordinate of circle center
-        // cy is y-coordinate of circle center
-     // r is radius
-        // fill is fill color
-    // polygon has attributes of points, fill
-        // takes a list of x,y coordinate pairs separated by spaces
-        // ew, weird to work with
-    // rectangle has attributes x, y, width, height, fill
-        // x is x-coordinate of top-left corner
-        // y is y of top-left
-        // width and height are semantic, like fill
-    // All the same, I had ChatGPT design the shapes to ensure they fit within the required 300x200px image. I understand the circle and square well enough to try it myself, but the triangle is witchcraft as far as I can tell. ¯\_(ツ)_/¯
+    const svg = createSVG().size(300, 200);
 
+    let shape;
     switch (logoShape) {
-        case "cirlce":
-            svgTemplate = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
-            break;
-        case "triangle":
-            svgTemplate = `<polygon points="150,25 75,175 225,175" fill="${shapeColor}" />`;
-            break;
-        case "square":
-            svgTemplate = `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`;
-            break;
-        default:
-            break;
+        case "circle":
+        shape = svg.circle(100)
+        break;
     }
-
-    // Now for a string that represents the user's desired logo based on choices
-    const svgString = `
-        
-    `;
 }
