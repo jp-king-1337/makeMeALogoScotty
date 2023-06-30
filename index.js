3df5acd8d8836604389578d2344401e4a927ad7a
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-30 17:36:02 -0400

    Added index.js pseudocode for prompts that I'll need to build

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const inquirer = require("inquirer");
+
+
+// Time for prompts
+// Text up to 3 characters
+
+
+// text color by keyword or hex number
+
+
+// shape with list: circle, triangle, square
+
+
+// shape color by keyword or hex number
