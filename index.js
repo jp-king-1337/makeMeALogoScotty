5efeb742730d7359723855e3ff39e2139865c934
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-30 23:27:06 -0400

    I am unsure of the issue. Can't seem to fix the bug. Going to try reworking it without using svg.js

--- index.js
+++ index.js
@@ -31,9 +31,14 @@ inquirer
         const logo = generateLogo(answers);
 
         saveLogoFile("logo.svg", logo)
-            .then(() => {
-                console.log("Logo saved as logo.svg.");
-            })
+        .then(() => {
+            console.log("Logo saved as logo.svg.");
+
+            // Inject the generated SVG code into the div element
+            const svgContent = fs.readFileSync("logo.svg", "utf8");
+            const logoDiv = document.querySelector(".logo");
+            logoDiv.innerHTML = svgContent;
+        })
             .catch((err) => {
                 console.error("An error occurred while saving the logo:", err);
             });
