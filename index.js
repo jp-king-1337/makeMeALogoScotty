aea07bb203f76fc0f8bf96a1e4c37c54e20a7928
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-30 23:41:04 -0400

    Removed node_modules, removed all dependencies but jest and inquirer. Going to try and remake without svg.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const inquirer = require("inquirer");
 const { generateLogo, saveLogoFile } = require("./lib/shapes");
 
@@ -33,11 +32,6 @@ inquirer
         saveLogoFile("logo.svg", logo)
         .then(() => {
             console.log("Logo saved as logo.svg.");
-
-            // Inject the generated SVG code into the div element
-            const svgContent = fs.readFileSync("logo.svg", "utf8");
-            const logoDiv = document.querySelector(".logo");
-            logoDiv.innerHTML = svgContent;
         })
             .catch((err) => {
                 console.error("An error occurred while saving the logo:", err);
